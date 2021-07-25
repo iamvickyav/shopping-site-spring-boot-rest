b4ab480a53804ac7908e1023ff39c6f0e047e71f
iamvickyav <vickyavw.10@gmail.com>
2021-07-25 12:42:08 +0530

    initial commit

---
+++ src/main/java/com/madhan/shopping/ProductResponse.java
@@ -0,0 +1,49 @@
+package com.madhan.shopping;
+
+public class ProductResponse {
+    private Integer id;
+    private String name;
+    private String desc;
+    private Double price;
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
